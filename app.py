aed63bbd4cb5f4d2ce656ccf57fe3153b7074172
himanshu-bist-hb <himanshubist858@gmail.com>
2025-07-27 23:40:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 import streamlit as st
 
-st.title("You are the most beautiful girl in the world")
+st.set_page_config(page_title="For You ❤️", page_icon="🌹", layout="centered")
 
+# Title
+st.title("Hi Love! 💖")
+
+# Image
+st.image("srishti.jpeg", caption="You are beautiful 😘", use_column_width=True)
+
+# Message
+st.markdown("""
+### Just wanted to say...
+You light up my world like nobody else ✨  
+Your smile, your kindness, and your love mean everything to me.  
+**I'm lucky to have you. ❤️**
+
+""")
+
+# Button
+if st.button("Click me 💌"):
+    st.success("I love you more every day 💕")
