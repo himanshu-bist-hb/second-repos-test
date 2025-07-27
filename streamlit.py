50333d5ba03705a1c23bca7f5b08deade288c47f
himanshu-bist-hb <himanshubist858@gmail.com>
2025-07-27 22:04:13 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,17 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 # Title
 st.title("My First Streamlit App")
 
-# Description
-st.write("This is a simple web app using Streamlit!")
-
-# Text Input
-name = st.text_input("Enter your name:", "Himanshu")
-st.write(f"Hello, {name}! 👋")
-
-# Slider
-number = st.slider("Pick a number", 1, 100)
-st.write(f"You selected: {number}")
